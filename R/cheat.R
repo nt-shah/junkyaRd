fc62047780e4bcd97703b016a1f866c88a82b2f1
nt-shah <99809769+nt-shah@users.noreply.github.com>
2023-01-27 19:24:09 +0100

    corrected cheat() script

--- R/cheat.R
+++ R/cheat.R
@@ -2,83 +2,46 @@
 # which gives you answers to specific questions off your assignment!
 
 # Works for Assignment 3.1
-# For questions: 4, 6, 8, 11, 17, 18, 31
+# For questions: 1, 9 and 16
 
 
 cheat <- function(question_number) {
 
-  # installing required packages
-  if(!require("stringr")) {
-    install.packages("stringr")
-    library(stringr)
-  }
+  # Question 3.1.1
+  if (question_number == 1) {
 
-  # Question 3.1.4
-  if (question_number == 4) {
+    ("Answer to Question 3.1.1 -----------------------------------------------") %>% noquote() %>% print()
+    ("set.seed(1)") %>% noquote() %>% print()
+    ("grades <- rnorm(n = 60, mean = 8, sd = 1)") %>% noquote() %>% print()
+    ("grades[grades > 10] <- 10") %>% noquote() %>% print()
+    ("grades[grades < 6] <- 6") %>% noquote() %>% print()
 
-    ("Answer to Question 3.1.4 -----------------------------------------------") %>% noquote() %>% print()
-    ("# Session > Set Working Directory > Choose Directory...") %>% noquote %>% print()
-
-  }
-
-  # Question 3.1.6
-  if (question_number == 6) {
-
-    ("Answer to Question 3.1.6 -----------------------------------------------") %>% noquote() %>% print()
-    ("pi_day <- c(3, pi, 'pi')") %>% noquote() %>% print()
-    ("pi_day  # you can see that all values get coerced to character") %>% noquote() %>% print()
+    ("hist(grades, xlim = range(0, 10)) # Histogram of simulated scores for course") %>% noquote %>% print()
 
   }
 
-  # Question 3.1.8
-  if (question_number == 8) {
+  # Question 3.1.9
+  if (question_number == 9) {
 
-    ("Answer to Question 3.1.8 -----------------------------------------------") %>% noquote() %>% print()
-    ("Part (a) ------------------------------------------") %>% noquote() %>% print()
+    ("Answer to Question 3.1.9 -----------------------------------------------") %>% noquote() %>% print()
+    ("install.packages('ggstatsplot') # install package ggstatsplot") %>% noquote() %>% print()
+    ("library(ggstatsplot)") %>% noquote() %>% print()
 
-    q31_a <- ("# It tells us that R executes mathematical operations in the common order
-          # of PEMDAS: Parentheses, Exponents, Multiplication, and Division (from
-          # left toright), Addition and Subtraction (from left to right)")
-    str_replace_all(q31_a, "[\r\n]" , "") %>% noquote %>% print()
+    ("tooth_growth <- ToothGrowth  # Tooth growth data set") %>% noquote() %>% print()
 
-    ("Part (b) -----------------------------------------------") %>% noquote() %>% print()
-    ("(4 + 6 * 2 + (4 + 6) * 2) / 10") %>% noquote %>% print()
+    ("ggbetweenstats(tooth_growth, x = supp, y = len, plot.type = 'boxviolin') # plot") %>% noquote() %>% print()
 
-  }
-
-  # Question 3.1.11
-  if (question_number == 11) {
-
-    ("Answer to Question 3.1.11 -----------------------------------------------") %>% noquote() %>% print()
-    ("# gives NA because sample_scores contains missing values") %>% noquote() %>% print()
-    ("# Fix this by specifying na.rm = TRUE:") %>% noquote() %>% print()
-    ("var(sample_scores, na.rm = TRUE)") %>% noquote() %>% print()
-    ("Answer: # variance = 9.7") %>% noquote() %>% print()
+    text_q9 <- ("# It appears that teeth growth is better when taking vitamin C through orange juice (OJ).The mean
+                # length (tooth growth) is larger than that of VC (vitamin C) medicines.")
+    str_replace_all(text_q9, "[\r\n]" , "") %>% noquote() %>% print()
 
   }
 
-  # Question 3.1.17
-  if (question_number == 17) {
-
-    ("Answer to Question 3.1.17 -----------------------------------------------") %>% noquote() %>% print()
-    ("# length()") %>% noquote() %>% print()
-
-  }
-
-  # Question 3.1.18
-  if (question_number == 18) {
-
-    ("Answer to Question 3.1.18 -----------------------------------------------") %>% noquote() %>% print()
-    ("install.packages('lme4')") %>% noquote() %>% print()
-    ("library(lme4)") %>% noquote() %>% print()
-
-  }
-
-  # Question 3.1.31:
-  if (question_number == 31) {
+  # Question 3.1.16:
+  if (question_number == 16) {
 
     ("Answer to Question 3.1.31 ----------------------------------------------") %>% noquote() %>% print()
-    ("array(NA, c(4,3,5)") %>% noquote() %>% print()
+    ("matrix(data = 1:9, nrow = 3, byrow = TRUE) * rep(seq(3), 3)") %>% noquote() %>% print()
 
   }
 
